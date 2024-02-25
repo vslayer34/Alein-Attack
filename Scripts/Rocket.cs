fc21b1f9ec2d0ff121ab21f5a9df5274fbc7ca01
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-25 04:37:48 +0200

    added Rocket to launch with lazt rocket effect

---
+++ Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class Rocket : Area2D
+{
+	[ExportGroup("Rocked Properties")]
+	[Export]
+	private float _speed;
+
+	float _time = 0.0f;
+	float _accerelationDuration = 1.0f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+		LaunchRocket((float)delta);
+    }
+
+
+	/// <summary>
+	/// Launch a rocket with lazy launch effect
+	/// </summary>
+	/// <param name="delta"></param>
+	private void LaunchRocket(float delta)
+	{
+		float _launchSpeed;
+
+		while (_time <= _accerelationDuration)
+		{
+			_launchSpeed = Mathf.Lerp(0.0f, _speed / 4.0f, _time / _accerelationDuration);
+			_time += delta;
+			GD.Print("Time: " + _time);
+			GD.Print(_launchSpeed);
+			GlobalPosition += Vector2.Right * _launchSpeed;
+			return;
+		}
+
+		_launchSpeed = _speed;
+		
+		GlobalPosition += Vector2.Right * _launchSpeed;
+	}
+}
