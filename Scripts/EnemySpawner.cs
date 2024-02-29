e964ce1f725600338473e4f79756f841ff8abd08
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-01 01:58:30 +0200

    added the ability to spawn enemy ships at one of six predetermined spawn points

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -3,8 +3,33 @@ using System;
 
 public partial class EnemySpawner : Node2D
 {
+	[Export]
+	private PackedScene _enemyShipScene;
+
+	[Export]
+	private Node2D _spawnPoints;
+
+	// Reference to newly created enemy ship
+    private EnemyShip _enemyShip;
+
+    public override void _Ready()
+    {
+        base._Ready();
+		_spawnPoints = GetNode<Node2D>("SpawnPoints");
+    }
+
 	private void OnTimerTimeout()
 	{
 		GD.Print("Time is out");
+		_enemyShip = _enemyShipScene.Instantiate<EnemyShip>();
+
+		ChooseRandomSpawnPoint().AddChild(_enemyShip);
+	}
+
+	private Node ChooseRandomSpawnPoint()
+	{
+		var spawnPoints = _spawnPoints.GetChildren();
+
+		return spawnPoints.PickRandom();
 	}
 }
