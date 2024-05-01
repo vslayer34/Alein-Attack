d99a4ebc14361f41b0cbb2027dc5a0dd921b22d0
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-05-01 21:38:29 +0300

    Created and enemyship that follow a path in the level scene

---
+++ Scripts/PathEnemy.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class PathEnemy : Path2D
+{
+    private PathFollow2D _pathFollow;
+    private float _enemySpeed = 0.15f;
+
+
+
+    public override void _Ready()
+    {
+        _pathFollow = GetNode<PathFollow2D>("PathFollow2D");
+        _pathFollow.ProgressRatio = 0.0f;
+    }
+
+    
+    public override void _Process(double delta)
+    {
+        if (_pathFollow.ProgressRatio >= 1.0f)
+        {
+            QueueFree();
+        }
+
+        _pathFollow.ProgressRatio += _enemySpeed * (float)delta;
+    }
+}
